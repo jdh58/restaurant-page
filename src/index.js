46b4f0382a94c30532713dd206e5477aee8cea80
Jonathan Hawes <jhawes580@gmail.com>
2022-09-14 20:57:03 -0700

    Initialize project and bring in CSS styles

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles.css'
+
+
+const div = document.createElement('div');
+
+// Create the content 
+const content = document.createElement('div');
+content.setAttribute('id', 'content');
+document.querySelector('body').appendChild(content);
+
+
+
+
