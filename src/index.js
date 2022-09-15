44616bb2edd20a84340f7f17d6d88c77779c3948
Jonathan Hawes <jhawes580@gmail.com>
2022-09-14 23:40:57 -0700

    Adds page initialization and tab switching logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import './styles.css'
+import './styles.css';
+import { loadHome } from './home';
+import { loadHeader } from './header';
+import { loadLocation } from './location';
 
+// Initialize the homepage on first load
+loadHeader();
+loadHome();
 
-const div = document.createElement('div');
-
-// Create the content 
-const content = document.createElement('div');
-content.setAttribute('id', 'content');
-document.querySelector('body').appendChild(content);
-
-
-
-
+// Add event listeners to the navigation items that swap out the page's
+// main content for the selected page's.
+const location = document.querySelector('li[data-navitem="2"]');
+const home = document.querySelector('li[data-navitem="1"]');
+location.addEventListener('click', loadLocation);
+home.addEventListener('click', loadHome);
